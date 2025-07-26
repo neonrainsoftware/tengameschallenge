6d316f7dedccb02b50656f0224151308220b5656
neonrainsoftware <darikhovdev@proton.me>
2025-07-26 15:17:42 -0400

    added player movement and mouse hiding

--- breakout/breakout/scripts/game_logic/global.gd
+++ breakout/breakout/scripts/game_logic/global.gd
@@ -4,33 +4,33 @@ var ui_manager
 var level_manager : Array
 
 func _ready() -> void:
-    Signals.connect("on_victory", victory_screen)
-    Signals.connect("on_defeat", defeat_screen)
+	Signals.connect("on_victory", victory_screen)
+	Signals.connect("on_defeat", defeat_screen)
 
-    ui_manager = load("res://scripts/ui_scenes/playerui.tscn").new()
-    add_child(ui_manager)
+	# ui_manager = load("res://scripts/ui_scenes/playerui.tscn").new()
+	# add_child(ui_manager)
 
-    var player_manager : Resource = load("res://scenes/game_scenes/player.tscn")
-    var player : Node = player_manager.instantiate()
-    add_child(player)
+	# var player_manager : Resource = load("res://scenes/game_scenes/player.tscn")
+	# var player : Node = player_manager.instantiate()
+	# add_child(player)
 
-    var ball_manager : Resource = load("res://scenes/game_scenes/ball.tscn")
-    var ball : Node = ball_manager.instantiate()
-    add_child(ball)
+	var ball_manager : Resource = load("res://scenes/game_scenes/ball.tscn")
+	var ball : Node = ball_manager.instantiate()
+	add_child(ball)
 
-    if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
-        Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 func _process(delta: float) -> void:
-    pass
+	pass
 
 func victory_screen() -> void:
-    pass
+	pass
 
 func defeat_screen() -> void:
-    if Input.get_mouse_mode() != Input.MOUSE_MODE_VISIBLE:
-        Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	if Input.get_mouse_mode() != Input.MOUSE_MODE_VISIBLE:
+		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 # TODO: make sure this works with different levels
 func retry_game() -> void:
-    get_tree().reload_current_scene()
+	get_tree().reload_current_scene()
--- breakout/breakout/scripts/game_logic/player.gd
+++ breakout/breakout/scripts/game_logic/player.gd
@@ -1,17 +1,19 @@
-extends Node
+extends Node2D
 
 var player_lives : int = 3
 var player_score : int = 0
 var player_name : String
+var mouse_pos
 
 func _ready() -> void:
-    Signals.connect("on_score", increment_score)
+	Signals.connect("on_score", increment_score)
 
-    player_lives = 3
-    player_score = 0
+	player_lives = 3
+	player_score = 0
 
 func _process(delta: float) -> void:
-    pass
+	mouse_pos = get_global_mouse_position()
+	position.x = mouse_pos.x
 
 func increment_score(score : int) -> void:
-    player_score += score
+	player_score += score
