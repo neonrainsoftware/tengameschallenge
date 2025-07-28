ecd355932b13cb36f390b91b4bc9c4347dd70d7d
neonrainsoftware <darikhovdev@proton.me>
2025-07-28 15:18:21 -0400

    added initial player model

---
+++ frogger/scripts/game_logic/player.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+var player_lives : int = 3
+var player_name : String
+const SPEED : float = 500.0
+
+func _physics_process(delta: float) -> void:
+	var direction : Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	if Input.is_action_pressed("ui_right") || Input.is_action_pressed("ui_left"):
+		direction.y = 0
+	elif Input.is_action_pressed("ui_up") || Input.is_action_pressed("ui_down"):
+		direction.x = 0
+	else: 
+		direction = Vector2.ZERO
+
+	direction = direction.normalized()
+	velocity = (direction * SPEED)
+	move_and_slide()
+
+func move_player(pos : Vector2) -> void:
+	$PlayerCharacter.position.x = pos.x
